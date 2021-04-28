b1666c1dd1970900a384d5cae92f575aff9f98f9
Haardispro <62053026+Haardispro@users.noreply.github.com>
2021-04-28 17:22:41 +0530

    okeh

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A past tense package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Haardispro/demopackage",
+    url="https://github.com/Haardispro/module2",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
